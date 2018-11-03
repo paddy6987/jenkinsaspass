cbe557a8a34619fc720c8560f778fe940f4d04fa
paddy6987 <42741416+paddy6987@users.noreply.github.com>
2018-11-03 20:02:11 +0530

    Update installartifactory.ps1

--- installartifactory.ps1
+++ installartifactory.ps1
@@ -1,4 +1,5 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
+Import-Module BitsTransfer
 Start-BitsTransfer –Source 'https://jenkinsaspass.blob.core.windows.net/software/artifactory-oss-6.5.2.zip' -Destination 'D:\'
 Start-BitsTransfer –Source 'https://jenkinsaspass.blob.core.windows.net/software/jre-8u191-windows-x64.exe' -Destination 'D:\'
 
