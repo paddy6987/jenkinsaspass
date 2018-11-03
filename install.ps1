6ad0dd02c4881aeb73e8167e9dae39cd7be6e3f8
paddy6987 <42741416+paddy6987@users.noreply.github.com>
2018-11-04 01:03:54 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,6 +2,5 @@ Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 Start-BitsTransfer –Source "https://jenkinsaspass.blob.core.windows.net/software/jenkins.msi" -Destination D:\
 cd D:\
 Start-Process jenkins.msi /qn 
-Start-Sleep 60
 
 
