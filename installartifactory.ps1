0699091143d76c9f36966a63fcac3cecc1a1d7a9
paddy6987 <42741416+paddy6987@users.noreply.github.com>
2018-11-03 22:21:50 +0530

    Update installartifactory.ps1

--- installartifactory.ps1
+++ installartifactory.ps1
@@ -1,2 +1,1 @@
-Start-BitsTransfer –Source 'https://jenkinsaspass.blob.core.windows.net/software/artifactory-oss-6.5.2.zip' -Destination 'D:\'
-Start-BitsTransfer –Source 'https://jenkinsaspass.blob.core.windows.net/software/jre-8u191-windows-x64.exe' -Destination 'D:\'
+Get-Date | Out-File D:\textzz.txt
