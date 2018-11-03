c983ac5381d4dd96935651e983bb52ed2a7ce819
paddy6987 <42741416+paddy6987@users.noreply.github.com>
2018-11-03 22:31:49 +0530

    Update installartifactory.ps1

--- installartifactory.ps1
+++ installartifactory.ps1
@@ -1,3 +1,2 @@
 Get-Date | Out-File D:\textzz.txt
-Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 Start-BitsTransfer –Source 'https://jenkinsaspass.blob.core.windows.net/software/artifactory-oss-6.5.2.zip' -Destination 'D:\'
