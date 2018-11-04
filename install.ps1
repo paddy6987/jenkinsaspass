87988677791f66bc516bb4996c7bb137ab9cea13
paddy6987 <42741416+paddy6987@users.noreply.github.com>
2018-11-04 17:01:06 +0530

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -2,7 +2,7 @@ $url = "https://jenkinsaspass.blob.core.windows.net/software/jenkins.msi"
 $drive = "D:\"
 Start-BitsTransfer –Source $url -Destination $drive
 Start-Sleep 60
- Start-Process D:\jenkins.msi /qn 
+ Start-Process D:\jenkins.msi /qn .
 
 
 
