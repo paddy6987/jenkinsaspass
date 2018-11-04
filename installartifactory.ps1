ff482d9efbba92512a86b8fe76cd1aed30e9b0a0
paddy6987 <42741416+paddy6987@users.noreply.github.com>
2018-11-04 15:43:58 +0530

    Update installartifactory.ps1

--- installartifactory.ps1
+++ installartifactory.ps1
@@ -1,47 +1,47 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-Start-BitsTransfer –Source "https://jenkinsaspass.blob.core.windows.net/software/jre-8u191-windows-x64.exe" -Destination "D:\"
-Start-BitsTransfer –Source "https://jenkinsaspass.blob.core.windows.net/software/artifactory-oss-6.5.2.zip" -Destination "D:\"
 
-$path = "C:\Program Files\Java"
+  Start-BitsTransfer –Source "https://jenkinsaspass.blob.core.windows.net/software/jre-8u191-windows-x64.exe" -Destination "D:\"
+  
+  Start-BitsTransfer –Source "https://jenkinsaspass.blob.core.windows.net/software/artifactory-oss-6.5.2.zip" -Destination "D:\"
 
-if (! (Test-path $path ))
-{
- 
- $Exe = "D:\jre-8u191-windows-x64.exe"
-$ExeArgs = @(
-   "INSTALL_SILENT=1"
-   "STATIC=0"
-   "AUTO_UPDATE=0"
-   "WEB_JAVA=1"
-   "WEB_JAVA_SECURITY_LEVEL=H"
-   "WEB_ANALYTICS=0"
-   "EULA=0"
-   "REBOOT=0"
-   "NOSTARTMENU=0"
-   "SPONSORS=0"
-)
-Start-Process $Exe -ArgumentList $ExeArgs -NoNewWindow -Wait 
+  $path = "C:\Program Files\Java"
 
-Start-Sleep 60
+  if (! (Test-path $path ))
+  {
+    $Exe = "D:\jre-8u191-windows-x64.exe"
+    $ExeArgs = @(
+    "INSTALL_SILENT=1"
+    "STATIC=0"
+    "AUTO_UPDATE=0"
+    "WEB_JAVA=1"
+    "WEB_JAVA_SECURITY_LEVEL=H"
+    "WEB_ANALYTICS=0"
+    "EULA=0"
+    "REBOOT=0"
+    "NOSTARTMENU=0"
+    "SPONSORS=0"
+    )
+  Start-Process $Exe -ArgumentList $ExeArgs -NoNewWindow -Wait 
 
-Add-Type -AssemblyName System.IO.Compression.FileSystem
-function unzip {
+  Start-Sleep 60
+
+  Add-Type -AssemblyName System.IO.Compression.FileSystem
+  function unzip {
    param( [string]$ziparchive, [string]$extractpath )
    [System.IO.Compression.ZipFile]::ExtractToDirectory( $ziparchive, $extractpath )
-}
-unzip "D:\artifactory-oss-6.5.2.zip" "D:\"
+    }
+  unzip "D:\artifactory-oss-6.5.2.zip" "D:\"
 
-D:\artifactory-oss-6.5.2\bin\run.vbs
+  D:\artifactory-oss-6.5.2\bin\run.vbs
 
- }
+  }
  else { 
  Add-Type -AssemblyName System.IO.Compression.FileSystem
-function unzip {
+ function unzip {
    param( [string]$ziparchive, [string]$extractpath )
    [System.IO.Compression.ZipFile]::ExtractToDirectory( $ziparchive, $extractpath )
-}
-unzip "D:\artifactory-oss-6.5.2.zip" "D:\"
+ }
+ unzip "D:\artifactory-oss-6.5.2.zip" "D:\"
 
-D:\artifactory-oss-6.5.2\bin\run.vbs
-
+ D:\artifactory-oss-6.5.2\bin\run.vbs
  }
