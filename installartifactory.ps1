9e9ef56d3c99e0d53e616b6c5e2b7af3490a7b3c
paddy6987 <42741416+paddy6987@users.noreply.github.com>
2018-11-04 12:14:00 +0530

    Update installartifactory.ps1

--- installartifactory.ps1
+++ installartifactory.ps1
@@ -1,7 +1,7 @@
 Import-Module BitsTransfer
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
-Start-BitsTransfer –Source "https://jenkinsaspass.blob.core.windows.net/software/artifactory-oss-6.5.2.zip" -Destination D:\
 Start-BitsTransfer –Source "https://jenkinsaspass.blob.core.windows.net/software/jre-8u191-windows-x64.exe" -Destination D:\
+Start-BitsTransfer –Source "https://jenkinsaspass.blob.core.windows.net/software/artifactory-oss-6.5.2.zip" -Destination D:\
 
 $path = "C:\Program Files\Java"
 
